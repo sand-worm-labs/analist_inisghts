abd8d788bb6951c53d3561128f868ade7d5bb73b
Angry Coding <mailidanielifechukwu@gmail.com>
2025-11-04 16:59:00 +0000

    chore: init

---
+++ src/collect.py
@@ -0,0 +1,7 @@
+# import requests
+
+def test():
+    print("Setup works!")
+
+if __name__ == "__main__":
+    test()
---
+++ src/config.py
@@ -0,0 +1,4 @@
+import os
+
+DUNE_API_KEY = os.getenv("DUNE_API_KEY")
+DATA_DIR = "data/"
