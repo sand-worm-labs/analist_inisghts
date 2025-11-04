39faf77a012fda4009ff35db53ea645a1fee7385
Angry Coding <mailidanielifechukwu@gmail.com>
2025-11-04 17:10:49 +0000

    chore: setup config file

--- src/config.py
+++ src/config.py
@@ -1,4 +1,16 @@
+from dotenv import load_dotenv
 import os
 
+load_dotenv() 
+
 DUNE_API_KEY = os.getenv("DUNE_API_KEY")
-DATA_DIR = "data/"
+DATA_PATH = os.getenv("DATA_PATH", "./data")
+DEBUG = os.getenv("DEBUG", "False").lower() == "true"
+
+def show_config():
+    print(f"DUNE_API_KEY={DUNE_API_KEY}")
+    print(f"DATA_PATH={DATA_PATH}")
+    print(f"DEBUG={DEBUG}")
+
+if __name__ == "__main__":
+    show_config()
